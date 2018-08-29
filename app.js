4ad613590aa24402682e1eed3e8b56198de3c58b
agungdh <agungdh@live.com>
2018-08-29 13:07:37 +0700

    update app.js
    + require mysql
    + connection
    + get /mahasiswa

--- app.js
+++ app.js
@@ -1,6 +1,28 @@
-const express = require('express')
-const app = express()
+var express = require('express')
+var app = express()
+
+var mysql = require('mysql')
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : '',
+  database : 'node_crud'
+});
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
 
 app.get('/', (req, res) => res.send('Hello World!'))
+
+app.get('/mahasiswa', (req, res) => {
+	connection.connect()
+
+	connection.query('SELECT * FROM mahasiswa', function (err, rows, fields) {
+	  if (err) throw err
+
+	  res.json(rows)
+	})
+
+	connection.end()
+})
+
+// app.get('/mahasiswa', (req, res) => res.send('Hello World!'))
