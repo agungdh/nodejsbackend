c75b5fe15b4021abe40162b0cd7be197506a3755
agungdh <agungdh@live.com>
2018-08-29 13:42:24 +0700

    update app.js
    tambah modul tambah

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 var express = require('express')
-var app = express()
-
 var mysql = require('mysql')
+var bodyParser = require('body-parser');
+
+var app = express()
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
@@ -14,15 +18,27 @@ app.listen(3000, () => console.log('Example app listening on port 3000!'))
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.get('/mahasiswa', (req, res) => {
-	connection.connect()
-
 	connection.query('SELECT * FROM mahasiswa', function (err, rows, fields) {
 	  if (err) throw err
 
 	  res.json(rows)
 	})
-
-	connection.end()
 })
 
-// app.get('/mahasiswa', (req, res) => res.send('Hello World!'))
+app.get('/mahasiswa/:npm', (req, res) => {
+	var params = req.params
+	connection.query('SELECT * FROM mahasiswa WHERE npm = ?', [params.npm], function (err, rows, fields) {
+	  if (err) throw err
+
+	  res.json(rows)
+	})
+})
+
+app.post('/mahasiswa', (req, res) => {
+	var body = req.body
+	connection.query('INSERT INTO mahasiswa SET ?', {'npm' : body.npm, 'nama' : body.nama, 'alamat' : body.alamat, 'tanggallahir' : body.tanggallahir, 'jeniskelamin' : body.jeniskelamin, }, function (err, rows, fields) {
+	  if (err) throw err
+
+	  res.json({'status' : true})
+	})
+})
